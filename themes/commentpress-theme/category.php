0883dd318f3510024ddb8a078c8d24253565df07
JackBlackLight <jd2148@columbia.edu>
2013-10-18 13:34:25 -0400

    changed call from the_excerpt to the_content

--- themes/commentpress-theme/category.php
+++ themes/commentpress-theme/category.php
@@ -43,7 +43,7 @@
 				<?php commentpress_echo_post_meta(); ?>
 			</div>
 
-			<?php the_excerpt() ?>
+			<?php the_content() ?>
 		
 			<p class="search_meta"><?php the_tags('Tags: ', ', ', '<br />'); ?> Posted in <?php the_category(', ') ?> | <?php edit_post_link('Edit', '', ' | '); ?>  <?php comments_popup_link('No Comments &#187;', '1 Comment &#187;', '% Comments &#187;'); ?></p>
 		
