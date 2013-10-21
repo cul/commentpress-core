6519ad0639cae26cbe25a3ad66d0edbadda85939
JackBlackLight <jd2148@columbia.edu>
2013-10-21 13:49:06 -0400

    set withcomments=1 before call to comments_template() so that comments render for category page

--- themes/commentpress-theme/assets/templates/comments_sidebar.php
+++ themes/commentpress-theme/assets/templates/comments_sidebar.php
@@ -25,7 +25,14 @@ if ( $post->comment_count == 0 ) { $no_comments_class = ' no_comments'; }
 
 <div class="sidebar_minimiser">
 
-<?php comments_template(); ?>
+<?php 
+
+global $withcomments;
+$withcomments = 1;
+
+comments_template(); 
+
+?>
 	
 </div><!-- /sidebar_minimiser -->
 
